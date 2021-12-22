ae7a2cbc9f40f6f17cae2f4d79c39fbe422dab1d
afarozo <grufik.k@gmail.com>
2021-12-22 22:26:49 +0100

    Animation adjustments(timing)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".main-h1").hide().fadeIn(2000),
-    $(".main-h2").hide().delay(1000).fadeIn(2000),
-    $(".main-h3").hide().delay(2000).fadeIn(2000),
-    $(".main-h4").hide().delay(3000).fadeIn(2000)
+    $(".main-h2").hide().delay(750).fadeIn(1500),
+    $(".main-h3").hide().delay(1500).fadeIn(1500),
+    $(".main-h4").hide().delay(2250).fadeIn(1500)
 });
