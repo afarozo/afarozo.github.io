bb179d7ee93d716422f080b9bc02903401264044
afarozo <grufik.k@gmail.com>
2021-12-22 19:22:47 +0100

    Fonts and nav adjustments

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(function(){
     $(".main-h1").hide().fadeIn(2000),
-    $(".main-h2").hide().delay(1000).fadeIn(2000)
+    $(".main-h2").hide().delay(1000).fadeIn(2000),
+    $(".main-h3").hide().delay(2000).fadeIn(2000),
+    $(".main-h4").hide().delay(3000).fadeIn(2000)
 });
